a1accfc35218d372d3bf4a3be50ca646416afb76
rrd108 <rrd@webmania.cc>
2024-06-10 17:30:06 +0200

    chore: comment out code for later

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,25 @@ import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand('vue-mess-detector.analyze', async () => {
-    const options = [
-      { label: 'No additional arguments', description: 'Run npx vue-mess-detector analyze', param: '' },
-      {
-        label: 'laksmi3',
-        description: 'Run npx vue-mess-detector analyze laksmi3',
-        param: '/home/rrd/public_html/laksmi3/src',
-      },
-    ]
+    // const options = [
+    //   { label: 'Analyze src folder', description: 'Run npx vue-mess-detector analyze', param: '' },
+    //   {
+    //     label: 'laksmi3',
+    //     description: 'Run npx vue-mess-detector analyze laksmi3',
+    //     param: '/home/rrd/public_html/laksmi3/src',
+    //   },
+    // ]
 
-    const selectedOption = await vscode.window.showQuickPick(options, {
-      placeHolder: 'Select an option for the analyze command',
-    })
+    // const selectedOption = await vscode.window.showQuickPick(options, {
+    //   placeHolder: 'Select an option for the analyze command',
+    // })
 
-    if (!selectedOption) {
-      return // User canceled the selection
-    }
+    // if (!selectedOption) {
+    //   return // User canceled the selection
+    // }
 
-    const command = `npx vue-mess-detector analyze ${selectedOption.param}`
+    // const command = `npx vue-mess-detector analyze ${selectedOption.param}`
+    const command = `npx vue-mess-detector analyze`
     const terminal = vscode.window.createTerminal('Vue Mess Detector')
     terminal.sendText(command)
     terminal.show()
